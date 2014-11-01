43c13dae34bb495843f322ec242c6e74de56c68b
Patrick Barker <patrickbarkerco@gmail.com>
2014-11-01 14:45:54 -0600

    updated java

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-yum update
+yum -y update
 
-yum groupinstall "X Window System"
+wget -O /opt/jdk-7u67-linux-x64.tar.gz --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz"
+tar xzf /opt/jdk-7u67-linux-x64.tar.gz -C /opt/
+touch /etc/profile.d/java.sh
+echo "export JAVA_HOME=/opt/jdk1.7.0_67" >> /etc/profile.d/java.sh
+echo "export JRE_HOME=/opt/jdk1.7.0_67/jre" >> /etc/profile.d/java.sh
+echo "export PATH=$PATH:/opt/jdk1.7.0_67/bin:/opt/jdk1.7.0_67/jre/bin" >> /etc/profile.d/java.sh
 
 yum install mysql-server -y
 service mysqld start
