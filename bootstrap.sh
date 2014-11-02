1327c080257bcacd38ef8d62b89687a4693dcf42
Patrick Barker <patrickbarkerco@gmail.com>
2014-11-02 15:18:15 -0700

    Added vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,10 @@
 
 yum -y update
 
+#get vim
+yum -y install vim
+echo "syntax on" > .vimrc
+
 #install oracle java
 wget -O /opt/jdk-7u67-linux-x64.tar.gz --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz"
 tar xzf /opt/jdk-7u67-linux-x64.tar.gz -C /opt/
